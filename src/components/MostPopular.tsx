b48ae444e5bc4ba9be980934a381b994e3daea45
junvely <junvely97@gmail.com>
2023-09-21 19:18:08 +0900

    BugFix : LookPage의 MostPopular 슬라이드 컴포넌트에서 연속 드래그 시 마우스 이벤트가 중단되는 문제 해결

--- src/components/MostPopular.tsx
+++ src/components/MostPopular.tsx
@@ -16,23 +16,22 @@ const MostPopular: React.FC<IMostPopularProps> = ({
   const slideLength = data ? Math.ceil(data.length / displaySlide) : 0;
   const [currentPage, setCurrentPage] = useState<number>(0);
 
-  //   PC 슬라이드 드래그 이벤트
+  // PC 슬라이드 드래그 이벤트
   const [startPoint, setStartPoint] = useState<number>(0);
 
   const handleDragStart = (e: React.MouseEvent<HTMLDivElement>) => {
     setStartPoint(e.pageX);
+    e.preventDefault();
   };
+
   const handleDragEnd = (e: React.MouseEvent<HTMLDivElement>) => {
-    console.log('hey');
-    console.log('e.pageX', e.pageX);
     if (startPoint > e.pageX) {
       nextSlide();
     } else if (startPoint < e.pageX) {
       prevSlide();
     }
+    e.preventDefault();
   };
-
-  console.log('startPoint:', startPoint, 'currentPage:', currentPage);
 
   const nextSlide = () => {
     if (currentPage < slideLength - 1) {
@@ -50,7 +49,7 @@ const MostPopular: React.FC<IMostPopularProps> = ({
     <div className='w-full overflow-hidden px-2'>
       <h3 className='font-semibold px-2 pt-1 pb-2'>{title}</h3>
       <div
-        className='w-full overflow-hidden select-none'
+        className='w-full overflow-hidden'
         onMouseDown={handleDragStart}
         onMouseUp={handleDragEnd}
       >
